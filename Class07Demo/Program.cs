6d4b760242fa86b3c5ec791bd4c1bff21151f047
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-16 15:00:02 -0700

    Add project files.

---
+++ Class07Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class07Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
